532a1e25072082bd2ca75985b54eef9b3cb744a8
gkk <gkk.dev.ops@gmail.com>
2023-01-22 19:09:52 +0100

    Finished CreditCardCheckout

--- cc/src/components/CreditCardCheckout.vue
+++ cc/src/components/CreditCardCheckout.vue
@@ -9,62 +9,76 @@ export default {
 </script>
 <template>
     <div class="w-[968px] h-[600px] bg-white flex flex-row">
-		<div class="bg-red-200 text-gray-400 w-1/3">
-			<img src="/CreditCardCheckout/hm.png" alt="H&M logo">
+		<div class="bg-[#F1EAE9] text-gray-600 w-1/3 text-center">
+			<img src="/CreditCardCheckout/hm.png" alt="H&M logo" class="w-16 mb-4 mt-6 mx-auto">
 			<div>
-				<h4>
+				<h4 class="text-3xl mb-2">
 					YOUR ORDER
 				</h4>
 			</div>
-			<img src="/CreditCardCheckout/bag.png" alt="bag">
-			<div>
-				Coursele goes here
+			<div class="m-2">
+				<img src="/CreditCardCheckout/bag.png" alt="bag">
+			</div>
+			<div class="mt-4 mb-10 justify-center flex flex-row items-center gap-2">
+				<div class="w-1 h-1 rounded-lg bg-gray-400"></div>
+				<div class="w-1 h-1 rounded-lg bg-red-600"></div>
+				<div class="w-1 h-1 rounded-lg bg-gray-400"></div>
+				<div class="w-1 h-1 rounded-lg bg-gray-400"></div>
+				<div class="w-1 h-1 rounded-lg bg-gray-400"></div>
 			</div>
 			<div>
-				<h3>
+				<h3 class="text-xl">
 					TOTAL
-					$145.98
+					<div class="font-bold text-2xl">
+						$145.98
+					</div>
 				</h3>
 			</div>
 		</div>
-		<div class="text-gray-400 w-2/3 gap-3 flex flex-col font-bold p-24">
+		<div class="text-gray-400 w-2/3 gap-3 flex flex-col font-bold p-24 pt-16">
 			<div class="text-center">
 				<h3 class="text-3xl">PAY WITH CREDIT CARD</h3>
 			</div>
-			<div>
+			<div class="justify-center mx-auto flex flex-col gap-4">
 				<div class="mt-2">
-					<p>Name of Card Holder</p>
+					<p>Name of Card Holder:</p>
 					<input
 						type="text"
 						placeholder="Name"
-						class="bg-white p-2 border-2 border-gray-400">
+						class="bg-white p-4 border-2 border-gray-400 w-full rounded m-1">
 				</div>
 				<div>
 					<p>Credit Card Number</p>
 					<input
 						type="text"
 						placeholder="Card Number"
-						class="bg-white p-2 border-2 border-gray-400">
+						class="bg-white p-4 border-2 border-gray-400 w-full rounded m-1">
 				</div>
-				<div>
+				<div class="flex flex-row gap-2">
 					<div>
 						<p>Expiration Date</p>
-						<div class="flex flex-row gap-2">
+						<div class="flex flex-row">
 						<input
-							type="dropdown"
-							class="bg-white p-2 border-2 border-gray-400 w-16">
+							type="text"
+							placeholder="MM"
+							class="bg-white p-4 border-2 border-gray-400 w-16 rounded-tl rounded-bl m-1">
 						<input
-						type="dropdown"
-						class="bg-white p-2 border-2 border-gray-400 w-16">
+						type="text"
+						placeholder="YYYY"
+						class="bg-white p-4 border-2 border-gray-400 w-20 rounded-tr rounded-br m-1">
 						</div>
 					</div>
 					<div>
 						<p>CVC</p>
 						<input
 							type="text"
-							class="bg-white p-2 border-2 border-gray-400">
+							placeholder="CVV"
+							class="bg-white p-4 border-2 border-gray-400 w-32 rounded m-1">
 					</div>
 				</div>
+				<button class="mt-4 bg-[#bd2725] text-white p-4 rounded active:bg-red-400">
+					PAY SECURELY
+				</button>
 			</div>
 		</div>
 	</div>
